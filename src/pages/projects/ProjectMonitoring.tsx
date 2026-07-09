e62ffe2cc7ca7c6523443b53c3ed02561e0f6395
Nilver L <nilver.carhuapoma@unas.edu.pe>
2026-07-09 12:37:55 -0500

    Avances de login

--- src/pages/projects/ProjectMonitoring.tsx
+++ src/pages/projects/ProjectMonitoring.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState, useRef } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import {
   ArrowLeft,
